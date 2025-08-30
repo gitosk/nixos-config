d6553408c071839dfb3e166b1e78d0dad9e3b048
gitosk <oscarseiffert@gmail.com>
2025-08-30 23:50:09 +0200

    Flakes Setup with LibrePhoenix tutorial

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+
+
+
+  description = "My first flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; # Point to reepositories
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Syncronize versions
+  }; 
+
+  outputs = { self, nixpkgs, home-manager, ...}:  # This 3 expressions to pass lib into nixpkgs
+    let                                  
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {      # System configurations
+      nixos = lib.nixosSystem {  # "nixos is the Hostname"
+        inherit system;
+	modules = [ ./configuration.nix ];            # List of modules.nix files 
+      };
+    };
+    homeConfigurations = {       # user configurations
+      osk = home-manager.lib.homeManagerConfiguration { # "osk is the Hostname"
+	inherit pkgs;
+	modules = [ ./home.nix ];            # List of modules.nix files 
+      };
+    }; # What to do with them, the actual system
+  };
+
+}
+
