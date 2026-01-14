748f75f8239bd53fc984a383d44bfe92818a0faa
gitosk <oscarseiffert@gmail.com>
2026-01-14 21:07:59 +0100

    corrected home-manager version missmatch

--- flake.nix
+++ flake.nix
@@ -8,8 +8,8 @@
         
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; # Point to reepositories
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Syncronize versions
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+#    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Syncronize versions
     nvf.url = "github:NotAShelf/nvf";
     
     }; 
